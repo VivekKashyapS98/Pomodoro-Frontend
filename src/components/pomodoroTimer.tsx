7fe909dc4e78b8510a78e171a8672cc172d4861b
Vivek Kashyap <vivekyeskashyap@gmail.com>
2021-01-20 16:53:09 +0530

    timer added

--- src/components/pomodoroTimer.tsx
+++ src/components/pomodoroTimer.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 
 function PomodoroTimer({ running, tasks }: any) {
+  const [prev, setPrev] = useState<any>(0);
   const [sec, setSec] = useState<any>(0);
   const [min, setMin] = useState<any>(0);
   const [time, setTime] = useState<any>(`${min}:${sec}`);
@@ -9,23 +10,35 @@ function PomodoroTimer({ running, tasks }: any) {
   const taskTitle = tasks.find((task: { _id: any }) => task._id === running);
 
   useEffect(() => {
-    setInterval(function () {
-      if (sec === 60) {
-        setMin(min + 1);
-        setSec(0);
-      } else setSec(sec + 1);
-    }, 1000);
-  }, [min, sec]);
+    if (sec === 60) {
+      setMin(min + 1);
+      setSec(0);
+      setTime(`${min}:${sec}`);
+    } else {
+      setSec(sec + 1);
+      setTime(`${min}:${sec}`);
+    }
+    setSec(sec + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  });
 
   return (
     <div>
       <header>
         <nav className="padding d-flex flex-row justify-content-between">
-          <button className="navs btn btn-light">START</button>
+          <button
+            className="navs btn btn-light"
+            onClick={() => {
+              setMin(0);
+              setSec(0);
+            }}
+          >
+            START
+          </button>
           <button className="navs btn btn-light">STOP</button>
         </nav>
       </header>
-      <h2>Task: {taskTitle.title}</h2>
+      {taskTitle ? <h2>Task: {taskTitle.title}</h2> : null}
       <h2>{time}</h2>
     </div>
   );
